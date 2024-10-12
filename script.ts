45cfd303261f3b14fe37e02d63a75680161f28b3
umair7228 <umairnawaz7228@gmail.com>
2024-10-12 12:52:37 +0500

    static resume

---
+++ script.ts
@@ -0,0 +1,16 @@
+// script.js
+export function toggleSkills() {
+    const skillsSection = document.getElementById("skills");
+    const toggleButton = document.getElementById("toggle-skills");
+  
+    toggleButton?.addEventListener("click", () => {
+      if (skillsSection?.classList.contains("hidden")) {
+        skillsSection.classList.remove("hidden");
+        toggleButton.textContent = "Hide Skills";
+      } else {
+        skillsSection?.classList.add("hidden");
+        toggleButton.textContent = "Show Skills";
+      }
+    });
+  }
+  
